29b80b83e8852ff2e7cf9abbe72939ecd2822209
LightmanBro <omotosodavidromeo@gmail.com>
2023-01-11 10:23:20 +0100

    starteď working on the backend

--- php/mail.php
+++ php/mail.php
@@ -1,25 +1,24 @@
 <?php
-if(isset(['submit'])) {
-    $firstName = $_POST['firstNAme'];
-    $firstName = $_POST['lastName'];
-    $fromEmail = $_POST['email'];
-    // etc
+if (isset($_POST["submit"])) {
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $color = $_POST["color"];
 
-
-    // receipient email
-    $mailTo = '';
-
-    $subject = '.........etc' .$firstNAme;
-
-
-    $htmlContent = '<h2> Email Request Received </h2>
-    <p>Client NAme: </b>' .$firstName . " " . $lastName . '</p>'
-    '<p><b> Email</b>' .$firstName . " " . $lastName . '</p>'
-    // etc
-
-    $headers = "From: " .$firstNAme . "<". $fromEmail . ">";
-    $headers .= "MIME-Version: 1.0!"
+    if(!empty($firstName) && !empty($lastName) && !empty($email) && !empty($color)){
+        // mysqli_connect("hostName", "db_User", "db_password", "db_name")
+        $link = mysqli_connect("localhost", "root", "", "register");
+        if ($link == false) {
+            die(mysqli_connect_error());
+        }
+        $sql = "INSERT INTO Users (firstName, lastName, email, color) VALUES ('$firstName','$lastName', '$email','$color')";
+        if (mysqli_query($link, $sql)) {
+            echo "Record is succesfully inserted";
+        }else {
+            echo "something went wrong";
+        }
+    } else {
+        echo "Please provide all information";
+    }
 }
-
-
-?>
+ ?>
